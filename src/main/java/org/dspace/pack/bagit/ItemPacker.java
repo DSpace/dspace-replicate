ce8e14a42207fa3c8349a2a617bdb6ce1a63be84
Michael Ritter <emericaskate15@gmail.com>
2020-03-12 14:02:32 -0600

    Add license and javadoc

--- src/main/java/org/dspace/pack/bagit/ItemPacker.java
+++ src/main/java/org/dspace/pack/bagit/ItemPacker.java
@@ -21,8 +21,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import com.google.common.collect.ImmutableMap;
 import org.dspace.authorize.AuthorizeException;
