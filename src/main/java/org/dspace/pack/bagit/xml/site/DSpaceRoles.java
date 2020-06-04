1b9de7134c312ab65d5977f73b69db90784ca0a1
Michael Ritter <emericaskate15@gmail.com>
2020-06-04 13:30:54 -0600

    Convert Lists to Sets for unique entries

--- src/main/java/org/dspace/pack/bagit/xml/site/DSpaceRoles.java
+++ src/main/java/org/dspace/pack/bagit/xml/site/DSpaceRoles.java
@@ -1,7 +1,10 @@
 package org.dspace.pack.bagit.xml.site;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -9,42 +12,42 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "DSpaceRoles")
 public class DSpaceRoles {
 
-    private List<AssociatedGroup> groups;
-    private List<Person> people;
+    private Set<AssociatedGroup> groups;
+    private Set<Person> people;
 
     @XmlElement(name = "Group")
     @XmlElementWrapper(name = "Groups")
-    public List<AssociatedGroup> getGroups() {
+    public Set<AssociatedGroup> getGroups() {
         return groups;
     }
 
     public void addGroup(final AssociatedGroup group) {
         if (groups == null) {
-            groups = new ArrayList<>();
+            groups = new HashSet<>();
         }
 
         groups.add(group);
     }
 
-    public void setGroups(final List<AssociatedGroup> groups) {
+    public void setGroups(final Set<AssociatedGroup> groups) {
         this.groups = groups;
     }
 
     @XmlElement(name = "Person")
     @XmlElementWrapper(name = "People")
-    public List<Person> getPeople() {
+    public Set<Person> getPeople() {
         return people;
     }
 
     public void addPerson(final Person person) {
         if (people == null) {
-            people = new ArrayList<>();
+            people = new HashSet<>();
         }
 
         people.add(person);
     }
 
-    public void setPeople(final List<Person> people) {
+    public void setPeople(final Set<Person> people) {
         this.people = people;
     }
 }
