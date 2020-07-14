590d581f7227fccbbafd72d59f0b147732f0cea0
Michael Ritter <emericaskate15@gmail.com>
2020-07-14 16:53:34 -0600

    Pack additional site metadata and a list of the top level communities

--- src/main/java/org/dspace/pack/bagit/SitePacker.java
+++ src/main/java/org/dspace/pack/bagit/SitePacker.java
@@ -6,6 +6,7 @@ import static org.dspace.pack.PackerFactory.OBJECT_ID;
 import static org.dspace.pack.PackerFactory.OBJECT_TYPE;
 import static org.dspace.pack.PackerFactory.OBJFILE;
 import static org.dspace.pack.PackerFactory.OWNER_ID;
+import static org.dspace.pack.bagit.BagItAipWriter.BAG_AIP;
 import static org.dspace.pack.bagit.BagItAipWriter.BAG_MAN;
 import static org.dspace.pack.bagit.BagItAipWriter.OBJ_TYPE_DELETION;
 import static org.dspace.pack.bagit.BagItAipWriter.PROPERTIES_DELIMITER;
@@ -20,8 +21,13 @@ import java.util.Map;
 
 import org.dspace.app.util.Util;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Community;
 import org.dspace.content.Site;
+import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.packager.PackageException;
+import org.dspace.content.packager.PackageUtils;
+import org.dspace.content.service.CommunityService;
+import org.dspace.curate.Curator;
 import org.dspace.pack.Packer;
 import org.dspace.pack.bagit.xml.roles.DSpaceRoles;
 
@@ -34,6 +40,8 @@ public class SitePacker implements Packer {
     private final Site site;
     private final String archFmt;
 
+    private final CommunityService communityService = ContentServiceFactory.getInstance().getCommunityService();
+
     public SitePacker(Site site, String archFmt) {
         this.site = site;
         this.archFmt = archFmt;
@@ -44,10 +52,9 @@ public class SitePacker implements Packer {
         final Map<String, List<String>> properties = new HashMap<>();
         // object.properties
         final List<String> objectProperties = new ArrayList<>();
-        objectProperties.add(BAG_TYPE + PROPERTIES_DELIMITER + BAG_MAN);
-        objectProperties.add(OBJECT_TYPE + PROPERTIES_DELIMITER + OBJ_TYPE_DELETION);
-        objectProperties.add(OBJECT_ID + PROPERTIES_DELIMITER + site.getID());
-        objectProperties.add(CREATE_TS + PROPERTIES_DELIMITER + System.currentTimeMillis());
+        objectProperties.add(BAG_TYPE + PROPERTIES_DELIMITER + BAG_AIP);
+        objectProperties.add(OBJECT_TYPE + PROPERTIES_DELIMITER + "site");
+        objectProperties.add(OBJECT_ID + PROPERTIES_DELIMITER + site.getHandle());
         properties.put(OBJFILE, objectProperties);
 
         // collect the DSpaceRoles
@@ -59,17 +66,19 @@ public class SitePacker implements Packer {
         }
 
         // human readable metadata
-        final List<String> siteProperties = new ArrayList<>();
-        siteProperties.add("Site-Handle" + PROPERTIES_DELIMITER + site.getHandle());
-        siteProperties.add("DSpace-Version" + PROPERTIES_DELIMITER + Util.getSourceVersion());
-        properties.put("dspace.properties", siteProperties);
+        final List<String> dspaceProperties = new ArrayList<>();
+        dspaceProperties.add("Site-Handle" + PROPERTIES_DELIMITER + site.getHandle());
+        dspaceProperties.add("DSpace-Version" + PROPERTIES_DELIMITER + Util.getSourceVersion());
+        properties.put("dspace.properties", dspaceProperties);
 
-        // members file
-        /*
-        if (members.size() > 0) {
-            properties.put("members", members);
+        // add all top level communities
+        final List<String> communities = new ArrayList<>();
+        final List<Community> allTopCommunities = communityService.findAllTop(Curator.curationContext());
+        for (Community community : allTopCommunities) {
+            final String packageName = PackageUtils.getPackageName(community, archFmt);
+            communities.add(community.getHandle() + PROPERTIES_DELIMITER + packageName);
         }
-         */
+        properties.put("communities", communities);
 
         return new BagItAipWriter(packDir, archFmt, properties)
             .withDSpaceRoles(dSpaceRoles)
