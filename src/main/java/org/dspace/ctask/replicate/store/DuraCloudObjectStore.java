8509d26fe3c35ee047e563cb26ddfd6a5a7266c7
Michael Ritter <mikejritter@users.noreply.github.com>
2020-10-14 15:00:21 -0600

    Retry failed uploads in DuraCloudContentStore (#56)
    Resolves: https://jira.lyrasis.org/browse/DS-2000
    
    Use Retrier when uploading content to DuraCloud
    Add configuration properties for retrying uploads
    
    Squashed commit details:
    * Wrap upload in a Retriable
    * Formatting updates
    * Add configuration properties for retry variables
    * Use try w/ resources when creating the FileInputStream
    * Add check for tar when creating the mime type
    * Shuffle init logic around to make testing easier
    * Add tests to verify Retrier works as expected

--- src/main/java/org/dspace/ctask/replicate/store/DuraCloudObjectStore.java
+++ src/main/java/org/dspace/ctask/replicate/store/DuraCloudObjectStore.java
@@ -7,6 +7,10 @@
  */
 package org.dspace.ctask.replicate.store;
 
+import static org.duracloud.common.retry.Retrier.DEFAULT_MAX_RETRIES;
+import static org.duracloud.common.retry.Retrier.DEFAULT_WAIT_BETWEEN_RETRIES;
+import static org.duracloud.common.retry.Retrier.DEFAULT_WAIT_MULTIPLIER;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -15,12 +19,15 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.dspace.services.ConfigurationService;
 import org.dspace.services.factory.DSpaceServicesFactory;
 import org.duracloud.client.ContentStore;
 import org.duracloud.client.ContentStoreManager;
 import org.duracloud.client.ContentStoreManagerImpl;
 import org.duracloud.common.model.Credential;
+import org.duracloud.common.retry.Retriable;
+import org.duracloud.common.retry.Retrier;
 import org.duracloud.domain.Content;
 import org.duracloud.error.ContentStoreException;
 import org.duracloud.error.NotFoundException;
@@ -34,20 +41,28 @@ import org.dspace.curate.Utils;
  *
  * @author richardrodgers
  */
-public class DuraCloudObjectStore implements ObjectStore
-{
-    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+public class DuraCloudObjectStore implements ObjectStore {
 
     // DuraCloud store
     private ContentStore dcStore = null;
 
-    public DuraCloudObjectStore()
-    {
+    // properties for retrying uploads
+    private int maxRetries = DEFAULT_MAX_RETRIES;
+    private int defaultWait = DEFAULT_WAIT_BETWEEN_RETRIES;
+    private int waitMultiplier = DEFAULT_WAIT_MULTIPLIER;
+
+    public DuraCloudObjectStore() {
+    }
+
+    @VisibleForTesting
+    protected void setContentStore(final ContentStore contentStore) {
+        this.dcStore = contentStore;
     }
 
     @Override
-    public void init() throws IOException
-    {
+    public void init() throws IOException {
+        final ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+
         // locate & login to Duracloud store
         ContentStoreManager storeManager =
             new ContentStoreManagerImpl(configurationService.getProperty("duracloud.host"),
@@ -57,118 +72,95 @@ public class DuraCloudObjectStore implements ObjectStore
             new Credential(configurationService.getProperty("duracloud.username"),
                            configurationService.getProperty("duracloud.password"));
         storeManager.login(credential);
-        try
-        {
+
+        maxRetries = configurationService.getIntProperty("duracloud.retry.max", DEFAULT_MAX_RETRIES);
+        defaultWait = configurationService.getIntProperty("duracloud.retry.wait", DEFAULT_WAIT_BETWEEN_RETRIES);
+        waitMultiplier = configurationService.getIntProperty("duracloud.retry.multiplier", DEFAULT_WAIT_MULTIPLIER);
+
+        try {
             //Get the primary content store (e.g. Amazon)
             dcStore = storeManager.getPrimaryContentStore();
-        }
-        catch (ContentStoreException csE)
-        {
-            throw new IOException("Unable to connect to the DuraCloud Primary Content Store. Please check the DuraCloud connection/authentication settings in your 'duracloud.cfg' file.", csE);
+        } catch (ContentStoreException csE) {
+            throw new IOException("Unable to connect to the DuraCloud Primary Content Store. Please check the " +
+                                  "DuraCloud connection/authentication settings in your 'duracloud.cfg' file.", csE);
         }
     }
 
     @Override
-    public long fetchObject(String group, String id, File file) throws IOException
-    {
+    public long fetchObject(final String group, final String id, final File file) throws IOException {
         long size = 0L;
-        try
-        {
+        try {
             Content content = dcStore.getContent(getSpaceID(group), getContentPrefix(group) + id);
 
             // Attempt to get size from request header
             String contentSizeHeader = content.getProperties().get(ContentStore.CONTENT_SIZE);
             try {
-                size = Long.valueOf(contentSizeHeader);
-            }
-            catch(NumberFormatException nfe) {
+                size = Long.parseLong(contentSizeHeader);
+            } catch(NumberFormatException nfe) {
                 // ignore - header was missing or not a valid Long. We will determine size below
             }
 
             // Open local file and download content into it
-            FileOutputStream out = new FileOutputStream(file);
-            InputStream in = content.getStream();
-            Utils.copy(in, out);
-            in.close();
-            out.close();
+            try (FileOutputStream out = new FileOutputStream(file);
+                 InputStream in = content.getStream()) {
+                Utils.copy(in, out);
+            }
 
             // If size could not be previously determined from request header, determine it from the downloaded file
             if (size == 0L) {
                 size = file.length();
             }
-        }
-        catch (NotFoundException nfE)
-        {
+        } catch (NotFoundException nfE) {
             // no object - no-op
-        }
-        catch (ContentStoreException csE)
-        {
+        } catch (ContentStoreException csE) {
             throw new IOException(csE);
         }
         return size;
     }
 
     @Override
-    public boolean objectExists(String group, String id) throws IOException
-    {
-        try
-        {
+    public boolean objectExists(String group, String id) throws IOException {
+        try {
             return dcStore.getContentProperties(getSpaceID(group), getContentPrefix(group) + id) != null;
-        }
-        catch (NotFoundException nfE)
-        {
+        } catch (NotFoundException nfE) {
             return false;
-        }
-        catch (ContentStoreException csE)
-        {
+        } catch (ContentStoreException csE) {
             throw new IOException(csE);
         }
     }
 
     @Override
-    public long removeObject(String group, String id) throws IOException
-    {
+    public long removeObject(String group, String id) throws IOException {
         // get metadata before blowing away
         long size = 0L;
-        try
-        {
+        try {
             Map<String, String> attrs = dcStore.getContentProperties(getSpaceID(group), getContentPrefix(group) + id);
-            size = Long.valueOf(attrs.get(ContentStore.CONTENT_SIZE));
+            size = Long.parseLong(attrs.get(ContentStore.CONTENT_SIZE));
             dcStore.deleteContent(getSpaceID(group), getContentPrefix(group) + id);
-        }
-        catch (NotFoundException nfE)
-        {
+        } catch (NotFoundException nfE) {
             // no replica - no-op
-        }
-        catch (ContentStoreException csE)
-        {
+        } catch (ContentStoreException csE) {
             throw new IOException(csE);
         }
         return size;
     }
 
     @Override
-    public long transferObject(String group, File file) throws IOException
-    {
+    public long transferObject(String group, File file) throws IOException {
         long size = 0L;
         String chkSum = Utils.checksum(file, "MD5");
         // make sure this is a different file from what replica store has
         // to avoid network I/O tax
-        try
-        {
-            Map<String, String> attrs = dcStore.getContentProperties(getSpaceID(group), getContentPrefix(group) + file.getName());
-            if (! chkSum.equals(attrs.get(ContentStore.CONTENT_CHECKSUM)))
-            {
+        try {
+            Map<String, String> attrs = dcStore.getContentProperties(getSpaceID(group),
+                                                                     getContentPrefix(group) + file.getName());
+            if (! chkSum.equals(attrs.get(ContentStore.CONTENT_CHECKSUM))) {
                 size = uploadReplica(group, file, chkSum);
             }
-        }
-        catch (NotFoundException nfE)
-        {
+        } catch (NotFoundException nfE) {
             // no extant replica - proceed
             size = uploadReplica(group, file, chkSum);
-        }
-        catch (ContentStoreException csE)
-        {
+        } catch (ContentStoreException csE) {
             throw new IOException(csE);
         }
         // delete staging file
@@ -176,83 +168,73 @@ public class DuraCloudObjectStore implements ObjectStore
         return size;
     }
 
-    private long uploadReplica(String group, File file, String chkSum) throws IOException
-    {
-        try
-        {
-            //@TODO: We shouldn't need to pass a hardcoded MIME Type. Unfortunately, DuraCloud,
-            // as of 1.3, doesn't properly determine a file's MIME Type. In future it should.
-            String mimeType = "application/octet-stream";
-            if(file.getName().endsWith(".zip"))
+    private long uploadReplica(final String group, final File file, final String chkSum) throws IOException {
+        try (final FileInputStream fileInputStream = new FileInputStream(file)) {
+            // get the mime type for DuraCloud
+            final String mimeType;
+            final String filename = file.getName();
+            if (filename.endsWith(".zip")) {
                 mimeType = "application/zip";
-            else if (file.getName().endsWith(".tgz"))
+            } else if (filename.endsWith(".tgz") || filename.endsWith(".gzip")) {
                 mimeType = "application/x-gzip";
-            else if(file.getName().endsWith(".txt"))
+            } else if (filename.endsWith(".txt")) {
                 mimeType = "text/plain";
+            } else if (filename.endsWith(".tar")) {
+                mimeType = "application/tar";
+            } else {
+                mimeType = "application/octet-stream";
+            }
 
-            dcStore.addContent(getSpaceID(group), getContentPrefix(group) + file.getName(),
-                               new FileInputStream(file), file.length(),
-                               mimeType, chkSum,
-                               new HashMap<String, String>());
+            new Retrier(maxRetries, defaultWait, waitMultiplier).execute(new Retriable() {
+                @Override
+                public String retry() throws Exception {
+                    return dcStore.addContent(getSpaceID(group), getContentPrefix(group) + filename,
+                                       fileInputStream, file.length(),
+                                       mimeType, chkSum,
+                                       new HashMap<String, String>());
+                }
+            });
 
             return file.length();
-        }
-        catch (ContentStoreException csE)
-        {
-            throw new IOException(csE);
+        } catch (Exception e) {
+            throw new IOException(e);
         }
     }
 
     @Override
-    public long moveObject(String srcGroup, String destGroup, String id) throws IOException
-    {
+    public long moveObject(String srcGroup, String destGroup, String id) throws IOException {
         // get file-size metadata before moving the content
         long size = 0L;
-        try
-        {
-            Map<String, String> attrs = dcStore.getContentProperties(getSpaceID(srcGroup), getContentPrefix(srcGroup) + id);
-            size = Long.valueOf(attrs.get(ContentStore.CONTENT_SIZE));
+        try {
+            Map<String, String> attrs = dcStore.getContentProperties(getSpaceID(srcGroup),
+                                                                     getContentPrefix(srcGroup) + id);
+            size = Long.parseLong(attrs.get(ContentStore.CONTENT_SIZE));
             dcStore.moveContent(getSpaceID(srcGroup), getContentPrefix(srcGroup) + id,
                                 getSpaceID(destGroup), getContentPrefix(destGroup) + id);
-        }
-        catch (NotFoundException nfE)
-        {
+        } catch (NotFoundException nfE) {
             // no replica - no-op
-        }
-        catch (ContentStoreException csE)
-        {
+        } catch (ContentStoreException csE) {
             throw new IOException(csE);
         }
         return size;
     }
 
     @Override
-    public String objectAttribute(String group, String id, String attrName) throws IOException
-    {
-        try
-        {
+    public String objectAttribute(String group, String id, String attrName) throws IOException {
+        try {
             Map<String, String> attrs = dcStore.getContentProperties(getSpaceID(group), getContentPrefix(group) + id);
 
-            if ("checksum".equals(attrName))
-            {
+            if ("checksum".equals(attrName)) {
                 return attrs.get(ContentStore.CONTENT_CHECKSUM);
-            }
-            else if ("sizebytes".equals(attrName))
-            {
+            } else if ("sizebytes".equals(attrName)) {
                 return attrs.get(ContentStore.CONTENT_SIZE);
-            }
-            else if ("modified".equals(attrName))
-            {
+            } else if ("modified".equals(attrName)) {
                 return attrs.get(ContentStore.CONTENT_MODIFIED);
             }
             return null;
-        }
-        catch (NotFoundException nfE)
-        {
+        } catch (NotFoundException nfE) {
             return null;
-        }
-        catch (ContentStoreException csE)
-        {
+        } catch (ContentStoreException csE) {
             throw new IOException(csE);
         }
     }
@@ -264,17 +246,17 @@ public class DuraCloudObjectStore implements ObjectStore
      * If the group contains a forward slash ('/'), then the substring
      * before the first slash is assumed to be the Space ID.
      * Otherwise, the entire group name is assumed to be the Space ID.
-     * @param String group name
+     * @param group name
      * @return DuraCloud Space ID
      */
-    private String getSpaceID(String group)
-    {
+    private String getSpaceID(String group) {
         //If group contains a forward or backslash, then the
         //Space ID is whatever is before that slash
-        if(group!=null && group.contains("/"))
+        if (group!=null && group.contains("/")) {
             return group.substring(0, group.indexOf("/"));
-        else // otherwise, the passed in group is the Space ID
+        } else { // otherwise, the passed in group is the Space ID
             return group;
+        }
     }
 
     /**
@@ -284,16 +266,16 @@ public class DuraCloudObjectStore implements ObjectStore
      * If the group contains a forward slash ('/'), then the substring
      * after the first slash is assumed to be the content naming prefix.
      * Otherwise, there is no content naming prefix.
-     * @param String group name
+     * @param group name
      * @return content prefix (ending with a forward slash)
      */
-    private String getContentPrefix(String group)
-    {
+    private String getContentPrefix(String group) {
         //If group contains a forward or backslash, then the
         // content prefix is whatever is after that slash
-        if(group!=null && group.contains("/"))
-            return group.substring(group.indexOf("/")+1) + "/";
-        else // otherwise, no content prefix is specified
+        if (group!=null && group.contains("/")) {
+            return group.substring(group.indexOf("/") + 1) + "/";
+        } else { // otherwise, no content prefix is specified
             return "";
+        }
     }
 }
---
+++ src/test/java/org/dspace/ctask/replicate/store/DuraCloudObjectStoreTest.java
@@ -0,0 +1,112 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.ctask.replicate.store;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+
+import org.duracloud.client.ContentStore;
+import org.duracloud.common.retry.Retrier;
+import org.duracloud.error.ContentStoreException;
+import org.duracloud.error.NotFoundException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+
+/**
+ * Tests for {@link DuraCloudObjectStore}. This creates a mock for the {@link ContentStore} so that we do not need to
+ * create outgoing requests to DuraCloud
+ */
+public class DuraCloudObjectStoreTest {
+
+    private final String group = "dc-object-store-test";
+    private final String mimeType = "application/zip";
+
+    private final ContentStore contentStore = mock(ContentStore.class);
+    private DuraCloudObjectStore objectStore;
+
+    @Before
+    public void setup() {
+        objectStore = new DuraCloudObjectStore();
+        objectStore.setContentStore(contentStore);
+    }
+
+    @Test
+    public void testUploadRetry() throws Exception {
+        final URL root = DuraCloudObjectStoreTest.class.getClassLoader().getResource("unpack");
+        // copy the file because the ObjectStore will delete it after transfer
+        final Path catalog = Paths.get(root.toURI()).resolve("catalog.zip");
+        final Path zipFile = Paths.get(root.toURI()).resolve("dc-upload.zip");
+        Files.copy(catalog, zipFile, StandardCopyOption.REPLACE_EXISTING);
+
+        assertThat(zipFile).exists();
+
+        // trigger uploadReplica
+        when(contentStore.getContentProperties(anyString(), anyString())).thenThrow(new NotFoundException("not found"));
+        // fail once then pass
+        when(contentStore.addContent(anyString(), anyString(), ArgumentMatchers.<InputStream>any(), anyLong(),
+                                     eq(mimeType), anyString(), ArgumentMatchers.<String, String>anyMap()))
+            .thenThrow(new ContentStoreException("first try fails"))
+            .thenReturn("second try succeeds");
+
+        objectStore.transferObject(group, zipFile.toFile());
+        assertThat(zipFile).doesNotExist();
+    }
+
+    @Test
+    public void testUploadFailure() throws Exception {
+        final URL root = DuraCloudObjectStoreTest.class.getClassLoader().getResource("unpack");
+        // copy the file because the ObjectStore will delete it after transfer
+        final Path catalog = Paths.get(root.toURI()).resolve("catalog.zip");
+        final Path zipFile = Paths.get(root.toURI()).resolve("dc-upload.zip");
+        Files.copy(catalog, zipFile, StandardCopyOption.REPLACE_EXISTING);
+
+        assertThat(zipFile).exists();
+
+        // trigger uploadReplica
+        when(contentStore.getContentProperties(anyString(), anyString())).thenReturn(new HashMap<String, String>());
+        // fail
+        when(contentStore.addContent(anyString(), anyString(), ArgumentMatchers.<InputStream>any(), anyLong(),
+                                     eq(mimeType), anyString(), ArgumentMatchers.<String, String>anyMap()))
+            .thenThrow(new ContentStoreException("exception"));
+
+        try {
+            objectStore.transferObject(group, zipFile.toFile());
+            fail("Expected transfer to fail");
+        } catch (IOException ignored) {
+        }
+
+        // verify that the retry process was attempted
+        final int numTries = 1 + Retrier.DEFAULT_MAX_RETRIES;
+        verify(contentStore, times(numTries)).addContent(anyString(), anyString(), ArgumentMatchers.<InputStream>any(),
+                                                         anyLong(), eq(mimeType), anyString(),
+                                                         ArgumentMatchers.<String, String>anyMap());
+
+        // we failed to upload, so the file should still exist
+        assertThat(zipFile).exists();
+        // and finish cleaning up
+        Files.delete(zipFile);
+    }
+
+}
